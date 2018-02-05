5938025882c11d6c017ce1f06dda9f86099d1c42
quangns <quangert1203@gmail.com>
2018-02-06 00:46:54 +0700

    them dialog sign up va log in sau khi click cac button

---
+++ main.js
@@ -0,0 +1,50 @@
+// Get the modal
+var modal = document.getElementById('myModal');
+
+var signup_modal = document.getElementsByClassName("modal-signup");
+
+var login_modal = document.getElementsByClassName("modal-login");
+
+var btn = document.getElementById("btnlogin");
+
+var login_tab = document.getElementById("login-tab");
+
+var signup_tab = document.getElementById("signup-tab");
+
+var span = document.getElementsByClassName("close")[0];
+
+function click_login() {
+    modal.style.display = "block";
+    signup_tab.style.background = "#e7e7e7";
+    login_tab.style.background = 'white';
+    for(var i = 0; i < signup_modal.length; i++) {
+        signup_modal[i].style.display="none";
+    }
+    for(var i = 0; i < login_modal.length; i++) {
+        login_modal[i].style.display="block";
+    }
+}
+
+function click_signup() {
+    modal.style.display = "block";
+    signup_tab.style.background = "white";
+    login_tab.style.background = "#e7e7e7";
+    for(var i = 0; i < signup_modal.length; i++) {
+        signup_modal[i].style.display="block";
+    }
+    for(var i = 0; i < login_modal.length; i++) {
+        login_modal[i].style.display="none";
+    }
+}
+
+// Wher the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
--- main.php
+++ main.php
@@ -6,7 +6,6 @@
     <title>Learn English</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
-    <script src="main.js"></script>
 </head>
 <body>
     <div class="first">
@@ -14,15 +13,14 @@
         <ul class="log sprite">
             <li class="signin"><a href="#">Contact</a></li>
             <li class="signin divider">|</li>
-            <li class="signin"><a href="#">Sign up</a></li>
-            <li class="signin"><button><b><a href="#"><span style="color: white; font-size:14px;"> Log in</span></a></b></button></li>
+            <li class="signin"><a href="#" onclick="click_signup()">Sign up</a></li>
+            <li class="signin"><button id="btnlogin" onclick="click_login()"><b><a href="#"><span style="color: white; font-size:14px;"> Log in</span></a></b></button></li>
         </ul>
-        
     </div>
     <div class="section1">
         <h1>Speak to learn English</h1>
         <h4>Connect with native speakers from UK or USA, and learners of English from around the world.</h4>
-        <button>Sign up - It's free</button>
+        <button onclick="click_signup()">Sign up - It's free</button>
     </div>
     <section class="s2main">
         <div class="section2 sectionleft">
@@ -42,5 +40,27 @@
             <p>Make friends with people from all over the world and learn new cultures every day.</p>
         </div>
     </section>
+    <section class="section4" id="myModal">
+        <div class="modal-content">
+            <div class="modal-header">
+                <div class="s4 s4left">
+                    <button id="signup-tab" onclick="click_signup()">Sign up</button>
+                </div>
+                <div class="s4 s4right">
+                    <button id="login-tab" onclick="click_login()">Log in</button>
+                </div>
+                <span class="s4right close">&times;</span>
+            </div>
+            <div class="modal-body">
+                <input class="modal-signup" type="text" name="yourname" placeholder="Your name">
+                <input type="text" name="yourmail" placeholder="Your mail"><br>
+                <input type="password" name="psw" placeholder="Password"><br>
+                <button class="forgotpw modal-login">Forgot your password?</button>
+                <button class="btnlog modal-signup">Sign up</button>
+                <button class="btnlog modal-login">Log in</button>
+            </div>
+        </div>
+    </section>
+    <script src="main.js"></script>
 </body>
 </html>
